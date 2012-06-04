86b5d82af4ded3f6c1958ae0b54fb5d22d2309a4
Petre Diaconescu <petrero@gmail.com>
2012-06-04 18:14:37 +0300

    any content that has no translation for specified language will fall back to default english in this case"

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module RailsReference325
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.i18n.fallbacks = true
   end
 end
