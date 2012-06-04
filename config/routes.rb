7dcfcb2ca56247039c0c9142789f7c16f96ce44e
Petre Diaconescu <petrero@gmail.com>
2012-06-04 12:59:49 +0300

    redirecting http://localhost:3000/path to http://localhost:3000/en/path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 RailsReference325::Application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
     resources :products
     root to: 'products#index'
   end
+  
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  match '', to: redirect("/#{I18n.default_locale}")
 end
