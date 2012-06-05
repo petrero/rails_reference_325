8a401476eba210584806aff9672db53831df4e62
Petre Diaconescu <petrero@gmail.com>
2012-06-05 21:38:23 +0300

    having data loaded by the rails application instead being served in the beginning

--- app/assets/javascripts/products.js.coffee
+++ app/assets/javascripts/products.js.coffee
@@ -6,3 +6,6 @@ jQuery ->
   $('#products').dataTable
     sPaginationType: "full_numbers"
     bJQueryUI: true
+    bProcessing: true
+    bServerSide: true
+    sAjaxSource: $('#products').data('source')
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    respond_to do |format|
+      format.html
+      format.json { render json: ProductsDatatable.new(view_context) } 
+    end
   end
 
   def show
