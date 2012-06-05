848483961509889b4aba74ac1b0bdb0d48f46507
Petre Diaconescu <petrero@gmail.com>
2012-06-05 18:05:13 +0300

    correcting the code after run the debugger

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :due_at, :name
   
   def self.due_on(date)
-    range = date.to_time...(date+1).to_time
+    range = date.beginning_of_day...date.end_of_day
     where(due_at: range)
   end
 end
