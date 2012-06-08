8472ebc0ee717e5fa59e17344f3180e2bcce514b
Petre Diaconescu <petrero@gmail.com>
2012-06-08 20:29:01 +0300

    adding fields for removing and editing answers from a question belonging a survey

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers
-  attr_accessible :content, :survey_id
+  attr_accessible :content, :survey_id, :answers_attributes
+  accepts_nested_attributes_for :answers, allow_destroy: true
 end
