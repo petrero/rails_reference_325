c55155c2a080b9d578aa3332cd0f5159384060d3
Petre Diaconescu <petrero@gmail.com>
2012-06-08 20:16:36 +0300

    adding a checkbox for deleting a question from a survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ActiveRecord::Base
   attr_accessible :name, :questions_attributes
   has_many :questions
-  accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :questions, allow_destroy: true
 end
