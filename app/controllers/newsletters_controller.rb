37338894c20b8ce1a5a4cc6f6c96ebd3422a1303
Petre Diaconescu <petrero@gmail.com>
2012-06-09 00:04:03 +0300

    moving the deliver process in the background

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,6 +1,6 @@
 class NewslettersController < ApplicationController
   def deliver
-    Newsletter.deliver(params[:id])
+    QC.enqueue "Newsletter.deliver", params[:id]
     redirect_to newsletters_url, notice: "Delivered newsletter."
   end
 
