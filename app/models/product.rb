24d1d8a6af875a0a52d36bca78ae5408e2e350a4
Petre Diaconescu <petrero@gmail.com>
2012-06-09 14:22:16 +0300

    storing runtime, author and rating into  a single properties hstore attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,17 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category, :description, :name, :price, :author
+  attr_accessible :category, :description, :name, :price
+  validates_numericality_of :runtime, allow_blank: true
   
   
   #store_accessor :properties, :author //is provided in Rails3.2 but is incompatible with the gem
-  
-  def author
-    properties && properties["author"]
-  end
-  
-  def author=(value)
-    self.properties = (properties || {}).merge("author" => value)
+  %w[author runtime rating].each do |key|
+    attr_accessible key
+    define_method(key) do
+      properties && properties[key]
+    end
+    
+    define_method("#{key}=") do |value|
+      self.properties = (properties || {}).merge(key => value)
+    end
   end
 end
