b446b469950c8dc7712871a803bdb5beef44e246
Petre Diaconescu <petrero@gmail.com>
2012-06-10 12:40:43 +0300

    adding validation for twitter_username format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :date_of_birth, :email, :github_username, :name, :password, :password_confirmation, :twitter_username, :website
-  has_secure_password  
+  has_secure_password 
+  
+  validates_format_of :twitter_username, without: /\W/, allow_blank: true 
 end
