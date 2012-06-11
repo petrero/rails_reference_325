e7008fb5d55d6261a93d8406205ed101c5fb8ceb
Petre Diaconescu <petrero@gmail.com>
2012-06-11 19:18:43 +0300

    providing a versioned rest api to our application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 RailsReference325::Application.routes.draw do
+  namespace :api do
+    # /api/... Api::
+    namespace :v1 do
+      resources :products
+    end
+  end
+
   resources :products
   root to: 'products#index'
 end
