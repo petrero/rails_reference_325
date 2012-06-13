cd7d356c21a622a1b36aa84a2c7c00e823bdeb83
Petre Diaconescu <petrero@gmail.com>
2012-06-13 23:30:30 +0300

    alternative solution for ScopeGenerator initializer module

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
-  generate_scopes
-  
-  scope :not_discontinued,   -> {discontinued_at_eq(nil) | discontinued_at_gt(Time.zone.now)}
-  scope :available,         -> {released_at_lteq(Time.zone.now).not_discontinued.stock_gteq(2)}
-  
-  
   def self.search(query)
-    available.where(arel_table[:name].matches("%#{query}%"))
+    match(
+      released_at: {lteq: Time.zone.now},
+      discontinued_at: [nil, {gt: Time.zone.now}],
+      stock: {gteq: 2},
+      name: {matches: "%#{query}%"}
+    )
   end
 end
