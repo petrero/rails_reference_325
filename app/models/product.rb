7d447696070eea39cff93cc961d5d90e569304bd
Petre Diaconescu <petrero@gmail.com>
2012-06-13 21:13:06 +0300

    do a hash of conditions in query search

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
   def self.search(query)
-    where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
+    where("released_at <= :now and (discontinued_at is null or discontinued_at > :now) and stock >= :stock and name like :query", now: Time.zone.now, stock: 2, query: "%#{query}%")
   end
 end
