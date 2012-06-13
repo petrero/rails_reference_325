509881a5c39b974bc99a9ade3eef34302e37d2e3
Petre Diaconescu <petrero@gmail.com>
2012-06-13 21:28:50 +0300

    move sections of the query in the named scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ActiveRecord::Base
   belongs_to :category
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
-  def self.search(query)
-    where("released_at <= :now and (discontinued_at is null or discontinued_at > :now) and stock >= :stock and name like :query", now: Time.zone.now, stock: 2, query: "%#{query}%")
-  end
+  scope :released,          -> {where("released_at <= ?", Time.zone.now)}
+  scope :not_discontinued,   -> {where("discontinued_at is null or discontinued_at > ?", Time.zone.now)}
+  scope :in_stock,          -> {where("stock >= ?", 2)}
+  scope :available,         -> {released.not_discontinued.in_stock}
+  scope :search,            ->(query) { available.where("name like ?", "%#{query}%") }
 end
