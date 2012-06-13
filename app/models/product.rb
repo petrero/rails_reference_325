4a118b1dee26179a180d87d59df5d9ed77dca5c2
Petre Diaconescu <petrero@gmail.com>
2012-06-13 20:44:27 +0300

    customize some sql behaviour

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,6 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
   def self.search(query)
-    where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
-    
     
     where do
       (released_at <= Time.zone.now) &
